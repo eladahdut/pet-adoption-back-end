5c7742742c8d50e284f5c9121b1fa085264bbb18
Elad Ahdut <elad_e_a@yahoo.com>
2021-04-13 20:36:46 +0300

    made some progress

---
+++ routes/signUp.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+/* POST signing up a new user. */
+router.post('/signup', function (req, res, next) {
+
+
+});
+
+module.exports = router;
