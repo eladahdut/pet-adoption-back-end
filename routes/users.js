1e51942a274ba164ffeec66f4c47364b2d8a0e78
Elad Ahdut <77791369+eladahdut@users.noreply.github.com>
2021-04-20 15:17:03 +0300

    still nothing

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get("/", function (req, res) {
+//   res.send("respond with a resource");
+// });
+
+/* POST signing up a new user. */
+router.post("/signup", function (req, res) {});
 
 module.exports = router;
