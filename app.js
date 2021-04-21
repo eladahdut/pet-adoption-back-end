c37abd8429089dc1786d9ed88f163f0a37ee73a4
Elad Ahdut <77791369+eladahdut@users.noreply.github.com>
2021-04-21 16:23:29 +0300

    created basic structure
    connected to mongoDB atlas

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-require('dotenv/config')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
 
-const postsRoute = require('./routes/posts')
+const postsRoute = require("./routes/posts");
 
-app.use('/posts', postsRoute)
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/posts", postsRoute);
 
 app.get("/", (req, res) => {
   res.send("we are on home");
 });
 
-
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => console.log("connected to db"))
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log("connected to DB")
+);
 
 app.listen(3000);
