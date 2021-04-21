fa00f06c363a1a5ad84b3f6719b67e5a527193f5
Elad Ahdut <elad_e_a@yahoo.com>
2021-04-21 07:37:58 +0300

    5800&TSID

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config')
+
+const postsRoute = require('./routes/posts')
+
+app.use('/posts', postsRoute)
 
 app.get("/", (req, res) => {
   res.send("we are on home");
 });
-app.get("/posts", (req, res) => {
-  res.send("we are on posts");
-});
+
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () => console.log("connected to db"))
 
 app.listen(3000);
