e92a5298ed3d42ee2ac1a533b6b349e446c4fdaa
Elad Ahdut <elad_e_a@yahoo.com>
2021-04-22 13:49:28 +0300

    posting to pets works, also get pets

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv/config");
+const mongoConnectToDB = require('./mongo/mongo')
 
 const postsRoute = require("./routes/posts");
-const petsRoute = require("./routes/pets");
+const petsRoute = require("./routes/petsNew");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/pets", petsRoute);
 app.use("/posts", postsRoute);
 
+
 app.get("/", (req, res) => {
   res.send("we are on home");
 });
 
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
-  console.log("connected to DB")
-);
+// mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+//   console.log("connected to DB")
+// );
 
 app.listen(3000);
