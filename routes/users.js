e82dc261801dd4e7e42f3987e563eec186e70b01
Elad Ahdut <77791369+eladahdut@users.noreply.github.com>
2021-04-26 15:09:21 +0300

    signup & login ready
    I think...

--- routes/users.js
+++ routes/users.js
@@ -1,37 +1,66 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/Users");
-const { registerValidation, loginValidation } = require('../validation')
+const { registerValidation, loginValidation } = require("../validation");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 //GET all users.
 router.get("/", async (req, res) => {
-    try {
-        const users = await User.find();
-        res.json(users);
-    } catch (err) {
-        res.json({ message: err });
-    }
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 //POST new user sign up.
 router.post("/signup", async (req, res) => {
-    //validate data before creating user
-    const { error } = registerValidation(req.body)
-    if (error) return res.status(400).send(error.details[ 0 ].message)
+  //validate data before creating user
+  const { error } = registerValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        phone: req.body.phone,
-        email: req.body.email,
-        password: req.body.password,
-        repeatPassword: req.body.repeatPassword
-    });
-    try {
-        const savedUser = await user.save();
-        res.json(savedUser);
-    } catch (err) {
-        res.json({ message: err });
-    }
+  //checking if the user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email already exists");
+
+  //Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashPassword = await bcrypt.hash(req.body.password, salt);
+  const hashRepeatPassword = await bcrypt.hash(req.body.repeatPassword, salt);
+
+  //Create a new user
+  const user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phone: req.body.phone,
+    email: req.body.email,
+    password: hashPassword,
+    repeatPassword: hashRepeatPassword,
+  });
+  try {
+    const savedUser = await user.save();
+    res.json(savedUser);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//Login
+router.post("/login", async (req, res) => {
+  //Validate date before user login
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  //Checking if the user exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Email not found");
+  //Password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("Invalid password");
+
+  //Create and assign a token
+  const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET);
+  res.header("auth-token", token).send(token);
 });
 
 // router.get("/:postId", async (req, res) => {
--- validation.js
+++ validation.js
@@ -1,48 +1,38 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 //Register validation
-const registerValidation = data => {
-    const schema = Joi.object({
-        firstName: Joi
-            .string()
-            .alphanum()
-            .min(2)
-            .max(30)
-            .required(),
-        lastName: Joi.string()
-            .alphanum()
-            .min(2)
-            .max(30)
-            .required(),
-        phone: Joi
-            .string()
-            .pattern(/^[0-9]+$/)
-            .required(),
-        email: Joi
-            .string()
-            .email({ minDomainSegments: 2, tlds: { allow: [ 'com', 'net' ] } })
-            .required(),
-        password: Joi
-            .string()
-            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
-        repeatPassword: Joi
-            .ref('password'),
-    });
-    return schema.validate(data)
-}
-const loginValidation = data => {
-    const schema = Joi.object({
-        email: Joi
-            .string()
-            .email({ minDomainSegments: 2, tlds: { allow: [ 'com', 'net' ] } })
-            .required(),
-        password: Joi
-            .string()
-            .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$'))
-            .required(),
-    });
-    return schema.validate(data)
-}
+const registerValidation = (data) => {
+  const schema = Joi.object({
+    firstName: Joi.string()
+      .required()
+      .min(2)
+      .max(50)
+      .regex(/^\w+(?:\s+\w+)*$/),
+    lastName: Joi.string().alphanum().min(2).max(30).required(),
+    phone: Joi.string()
+      .pattern(/^[0-9]+$/)
+      .required(),
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required(),
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required(),
+    repeatPassword: Joi.ref("password"),
+  });
+  return schema.validate(data);
+};
+const loginValidation = (data) => {
+  const schema = Joi.object({
+    email: Joi.string()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .required(),
+    password: Joi.string()
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .required(),
+  });
+  return schema.validate(data);
+};
 
 module.exports.registerValidation = registerValidation;
 module.exports.loginValidation = loginValidation;
@@ -50,5 +40,3 @@ module.exports.loginValidation = loginValidation;
 //validate data before creating user
 // const { error } = schema.validate(req.body);
 // if (error) return res.status(400).send(error.details[ 0 ].message)
-
-
